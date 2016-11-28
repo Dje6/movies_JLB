ad4384c20fdf1f00eb9720e4565f8ff4f70b5663
laurent76 <lau76120@gmail.com>
2016-11-28 10:14:11 +0100

    liens header vers php

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,7 @@
     <footer>
      <h3>MOVIES : notre super base de données de films</h3>
-      <p>Inscrivez-vous ici</p>
+      <p><a href="register.php">Inscrivez-vous ici</a></p>
+      <a href="connexion.php">Connectez-vous ici</a>
     </footer>
 
   <script type="text/javascript" src="assets/js/jquery-3.1.1.js"></script>
