3c5715d1bfab86fa2ff8fa3eb458c394d5b53ba0
Jessy <jessy35370@gmail.com>
2016-11-29 13:59:48 +0100

    php

--- movies_back.php
+++ movies_back.php
@@ -4,36 +4,39 @@ include('includes/functions.php');
 include('includes/header_back.php');
 if(isAdmin()) {
 
-  $sql = "SELECT id,title,year,rating FROM movies_full ORDER BY id";
+  $sql = "SELECT id,title,year,rating FROM movies_full ORDER BY title ASC";
   $query = $pdo->prepare($sql);
   $query->execute();
   $movies = $query->fetchAll();
 
 ?>
-  <table class="use table">
-    <tr>
-      <th>ID</th>
-      <th>Title</th>
-      <th>Year</th>
-      <th>Rating</th>
-      <th>Actions</th>
-    </tr>
+<div class="container">
+  <div class="row">
 
-<?php
+    <table class="use table">
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Year</th>
+        <th>Rating</th>
+        <th>Actions</th>
+      </tr>
 
-  foreach ($movies as $movie => $value) { ?>
+<?php foreach ($movies as $movie) { ?>
 
       <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><?php echo $movie['id']; ?></td>
+        <td><?php echo $movie['title']; ?></td>
+        <td><?php echo $movie['year']; ?></td>
+        <td><?php echo $movie['rating']; ?></td>
         <td></td>
       </tr>
 
-
-  <?php }
-  }else{
+  <?php } ?>
+    </table>
+  </div>
+</div>
+<?php }else{
       header('Location: index.php');
     }
 
