0629a1be5f0fc65a5059213321396cf966e2ab77
laurent76 <lau76120@gmail.com>
2016-11-29 16:56:31 +0100

    git de fin de journée

--- dashboard.php
+++ dashboard.php
@@ -68,163 +68,157 @@ $ratingfilm = $query->fetchColumn();
 				<h1 class="page-header">Dashboard</h1>
 			</div>
 		</div><!--/.row-->
-
-		<div class="row">
-			<div class="col-xs-12 col-md-6 col-lg-3">
-				<div class="panel panel-blue panel-widget ">
-					<div class="row no-padding">
-						<div class="col-sm-3 col-lg-5 widget-left">
-							<svg class="glyph stroked bag"><use xlink:href="#stroked-bag"></use></svg>
-						</div>
-						<div class="col-sm-9 col-lg-7 widget-right">
-							<div class="large"><?php echo $nb_Ajfilms ;?></div>
-							<div class="text-muted">Films ajoutés</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-12 col-md-6 col-lg-3">
-				<div class="panel panel-orange panel-widget">
-					<div class="row no-padding">
-						<div class="col-sm-3 col-lg-5 widget-left">
-							<svg class="glyph stroked empty-message"><use xlink:href="#stroked-empty-message"></use></svg>
-						</div>
-						<div class="col-sm-9 col-lg-7 widget-right">
-							<div class="large"><?php echo $nb_films ;?></div>
-							<div class="text-muted">Nombre de films Total</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-12 col-md-6 col-lg-3">
-				<div class="panel panel-teal panel-widget">
-					<div class="row no-padding">
-						<div class="col-sm-3 col-lg-5 widget-left">
-							<svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg>
-						</div>
-						<div class="col-sm-9 col-lg-7 widget-right">
-							<div class="large"><?php echo $nb_abo ;?></div>
-							<div class="text-muted">Nombre d'abonnés</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-12 col-md-6 col-lg-3">
-				<div class="panel panel-red panel-widget">
-					<div class="row no-padding">
-						<div class="col-sm-3 col-lg-5 widget-left">
-							<svg class="glyph stroked app-window-with-content"><use xlink:href="#stroked-app-window-with-content"></use></svg>
-						</div>
-						<div class="col-sm-9 col-lg-7 widget-right">
-							<div class="large">
-								<p><?php
-
-								if(file_exists('compteur_visites.txt'))
-								{
-												$compteur_f = fopen('compteur_visites.txt', 'r+');
-												$compte = fgets($compteur_f);
-								}
-								else
-								{
-												$compteur_f = fopen('compteur_visites.txt', 'a+');
-												$compte = 0;
-								}
-								if(!isset($_SESSION['compteur_de_visite']))
-								{
-												$_SESSION['compteur_de_visite'] = 'visite';
-												$compte++;
-												fseek($compteur_f, 0);
-												fputs($compteur_f, $compte);
-								}
-								fclose($compteur_f);
-								echo '<strong>'.$compte.'</strong> visites.';
-								?></p>
+					<div class="row">
+						<div class="col-xs-12 col-md-6 col-lg-3">
+							<div class="panel panel-blue panel-widget ">
+								<div class="row no-padding">
+									<div class="col-sm-3 col-lg-5 widget-left">
+										<svg class="glyph stroked bag"><use xlink:href="#stroked-bag"></use></svg>
+									</div>
+									<div class="col-sm-9 col-lg-7 widget-right">
+										<div class="large"><?php echo $nb_Ajfilms ;?></div>
+										<div class="text-muted">Films ajoutés</div>
+									</div>
+								</div>
 							</div>
-							<div class="text-muted">Nombre de visites</div>
 						</div>
-					</div>
-				</div>
-			</div>
-		</div><!--/.row-->
+						<div class="col-xs-12 col-md-6 col-lg-3">
+							<div class="panel panel-orange panel-widget">
+								<div class="row no-padding">
+									<div class="col-sm-3 col-lg-5 widget-left">
+										<svg class="glyph stroked empty-message"><use xlink:href="#stroked-empty-message"></use></svg>
+									</div>
+									<div class="col-sm-9 col-lg-7 widget-right">
+										<div class="large"><?php echo $nb_films ;?></div>
+										<div class="text-muted">Nombre de films Total</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-12 col-md-6 col-lg-3">
+							<div class="panel panel-teal panel-widget">
+								<div class="row no-padding">
+									<div class="col-sm-3 col-lg-5 widget-left">
+										<svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg>
+									</div>
+									<div class="col-sm-9 col-lg-7 widget-right">
+										<div class="large"><?php echo $nb_abo ;?></div>
+										<div class="text-muted">Nombre d'abonnés</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-12 col-md-6 col-lg-3">
+							<div class="panel panel-red panel-widget">
+								<div class="row no-padding">
+									<div class="col-sm-3 col-lg-5 widget-left">
+										<svg class="glyph stroked app-window-with-content"><use xlink:href="#stroked-app-window-with-content"></use></svg>
+									</div>
+									<div class="col-sm-9 col-lg-7 widget-right">
+										<div class="large">
+											<p><?php
 
-		<div class="row">
-			<div class="col-lg-12">
-				<div class="panel panel-default">
-					<div class="panel-heading">Statistiques du site</div>
-					<div class="panel-body">
-						<div class="canvas-wrapper">
-							<canvas class="main-chart" id="line-chart" height="200" width="600"></canvas>
+											if(file_exists('compteur_visites.txt'))
+											{
+															$compteur_f = fopen('compteur_visites.txt', 'r+');
+															$compte = fgets($compteur_f);
+											}
+											else
+											{
+															$compteur_f = fopen('compteur_visites.txt', 'a+');
+															$compte = 0;
+											}
+											if(!isset($_SESSION['compteur_de_visite']))
+											{
+															$_SESSION['compteur_de_visite'] = 'visite';
+															$compte++;
+															fseek($compteur_f, 0);
+															fputs($compteur_f, $compte);
+											}
+											fclose($compteur_f);
+											echo '<strong>'.$compte.'</strong> visites.';
+											?></p>
+										</div>
+										<div class="text-muted">Nombre de visites</div>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-			</div>
-		</div><!--/.row-->
-
-		<div class="row">
-			<div class="col-xs-6 col-md-3">
-				<div class="panel panel-default">
-					<div class="panel-body easypiechart-panel">
-						<h4 class="dash">Films notés plus de 90/100</h4>
-						<?php echo $ratingfilm; ?>
-						<div class="easypiechart" id="easypiechart-blue" data-percent="92" ><span class="percent">
-							<?php
-								$Pourcent =  Pourcentage($ratingfilm, 100);
-								echo $Pourcent.'%';
-								?></span>
+					</div><!--/.row-->
+					<div class="row">
+						<div class="col-lg-12">
+							<div class="panel panel-default">
+								<div class="panel-heading">Statistiques du site</div>
+								<div class="panel-body">
+									<div class="canvas-wrapper">
+										<canvas class="main-chart" id="line-chart" height="200" width="600"></canvas>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-6 col-md-3">
-				<div class="panel panel-default">
-					<div class="panel-body easypiechart-panel">
-						<h4 class="dash">Objectif Nombre de films enregistrés 10000</h4>
-						<?php echo $nb_films ;?>
-						<div class="easypiechart" id="easypiechart-orange"><span class="percent">
-							<?php
-								$Pourcent =  Pourcentage($nb_films, 10000);
-								echo $Pourcent.'%';
-								?></span>
+					</div><!--/.row-->
+					<div class="row">
+						<div class="col-xs-6 col-md-3">
+							<div class="panel panel-default">
+								<div class="panel-body easypiechart-panel">
+									<h4 class="dash">Films notés plus de 90/100</h4>
+									<?php echo $ratingfilm; ?>
+									<div class="easypiechart" id="easypiechart-blue"><span class="percent">
+										<?php
+											$Pourcent =  Pourcentage($ratingfilm, 100);
+											echo $Pourcent.'%';
+											?></span>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-6 col-md-3">
-				<div class="panel panel-default">
-					<div class="panel-body easypiechart-panel">
-						<h4 class="dash">Obhectif nombre d'abonnés 500</h4>
-						<p><?php echo $nb_abo; ?></p>
-						<div class="easypiechart" id="easypiechart-teal" ><span class="percent">
-							<?php
-								$Pourcent =  Pourcentage($nb_abo, 500);
-								echo $Pourcent.'%';
-								?></span>
+						<div class="col-xs-6 col-md-3">
+							<div class="panel panel-default">
+								<div class="panel-body easypiechart-panel">
+									<h4 class="dash">Objectif Nombre de films enregistrés 10000</h4>
+									<?php echo $nb_films ;?>
+									<div class="easypiechart" id="easypiechart-orange"><span class="percent">
+										<?php
+											$Pourcent =  Pourcentage($nb_films, 10000);
+											echo $Pourcent.'%';
+											?></span>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-6 col-md-3">
-				<div class="panel panel-default">
-					<div class="panel-body easypiechart-panel">
-						<h4 class="dash">Objectif Visiteurs 250/jour</h4>
-							<p><?php
-			      		echo '<strong>'.$compte.'</strong> visites.';
-			      		?></p>
-								<div class="easypiechart" id="easypiechart-red" ><span class="percent">
-							<?php
-								$Pourcent =  Pourcentage($compte, 250);
-								echo $Pourcent.'%';
-								?>
-							</span>
+						<div class="col-xs-6 col-md-3">
+							<div class="panel panel-default">
+								<div class="panel-body easypiechart-panel">
+									<h4 class="dash">Objectif nombre d'abonnés 500</h4>
+									<p><?php echo $nb_abo; ?></p>
+									<div class="easypiechart" id="easypiechart-teal" ><span class="percent">
+										<?php
+											$Pourcent =  Pourcentage($nb_abo, 500);
+											echo $Pourcent.'%';
+											?></span>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
+						<div class="col-xs-6 col-md-3">
+							<div class="panel panel-default">
+								<div class="panel-body easypiechart-panel">
+									<h4 class="dash">Objectif Visiteurs 250/jour</h4>
+										<p><?php
+						      		echo '<strong>'.$compte.'</strong> visites.';
+						      		?></p>
+											<div class="easypiechart" id="easypiechart-red" ><span class="percent">
+										<?php
+											$Pourcent =  Pourcentage($compte, 250);
+											echo $Pourcent.'%';
+											?>
+										</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div><!--/.row-->
 				</div>
-			</div>
-		</div><!--/.row-->
-
-
-				</div>
-
 			</div><!--/.col-->
 		</div><!--/.row-->
 	</div>	<!--/.main-->
