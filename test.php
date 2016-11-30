1561ae4784549b3ff5287aba188bb3e424fd699b
laurent76 <lau76120@gmail.com>
2016-11-30 15:35:12 +0100

    erreur

--- test.php
+++ test.php
@@ -7,6 +7,15 @@ include 'includes/functions.php';
 
 include 'includes/header.php';?>
 
+<svg version="1.1" id="fb-icon-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="34px" height="76.003px" viewBox="0 0 34 76.003" enable-background="new 0 0 34 76.003" xml:space="preserve">
+        <path fill="#FFFFFF" d="M9.904,76.001V46.57H0V35.101h9.904v-8.459c0-9.816,5.995-15.161,14.752-15.161
+                                    c4.193,0,7.801,0.312,8.852,0.452V22.19l-6.076,0.002c-4.76,0-5.682,2.264-5.682,5.585v7.323h11.355L31.629,46.57H21.75v29.432
+                                    H9.904z"></path>
+        <rect x="58.999" y="0" fill="none" width="76.003" height="76.003"></rect>
+        </svg>
+
+
+
 
 
 <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
