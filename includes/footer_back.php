8125e76249a92bb8d2dbb8079968bdcf204939b8
laurent76 <lau76120@gmail.com>
2016-11-30 12:21:35 +0100

    social liens

--- includes/footer_back.php
+++ includes/footer_back.php
@@ -3,7 +3,9 @@
     <footer>
 
     </footer>
-    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+    <script type="text/javascript" src="http://platform.linkedin.com/in.js"></script>
+    <script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>
+    <script type="text/javascript" src="https://apis.google.com/js/plusone.js">{lang: 'fr'}</script>
 
     <script src="assets/js/jquery-1.11.1.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
--- test.php
+++ test.php
@@ -9,10 +9,17 @@ include 'includes/header.php';?>
 
 
 
-<a name="fb_share" type="box_count" share_url="http://www.example.com/page.html"></a>
+<a href="https://twitter.com/share" class="twitter-share-button">Tweet</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+<iframe src="http://www.facebook.com/plugins/like.php?href=http://www.example.com/page.html&layout=box_count&show_faces=true&width=65&action=like&font=arial&colorscheme=light&height=65"
+  scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:65px; height:65px; margin-top:3px;" allowTransparency="true"></iframe>
+  <a name="fb_share" type="box_count" share_url="http://www.example.com/page.html"></a>
+
+<g:plusone size="tall"></g:plusone>
+<script type="in/share" data-counter="top"></script>
+
+
+<script type="text/javascript" src="http://platform.linkedin.com/in.js"></script>
 <script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>
-
-
-
-
+<script type="text/javascript" src="https://apis.google.com/js/plusone.js">{lang: 'fr'}</script>
 <?php include 'includes/footer.php'; ?>
